68c9eeb43169bceb46a95ca3ac6331998c5b57ee
wjb108 <williambenedict3@gmail.com>
2021-05-11 12:19:10 -0400

    debugging with corey

--- client/src/screens/ItemDetail/ItemDetail.jsx
+++ client/src/screens/ItemDetail/ItemDetail.jsx
@@ -1,16 +1,27 @@
 import "./ItemDetail.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getItem } from "../../services/items";
 
-export default function ItemDetail(props) {
-  const { allItems } = props;
+export default function ItemDetail() {
+  const [item, setItem] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  const fetchItem = async () => {
+    const item = await getItem(id);
+    console.log(item);
+    setItem(item);
+  };
+
   return (
     <div>
-      {allItems.map((item) => (
-        <div className="bazar-container" key={item.id}>
-          <img src={item.img_url} alt={item.title} />
-          <h4>{item.title}</h4>
-          <p>{item.short_description}</p>
-        </div>
-      ))}
+      <img src={item.img_url} alt={item.title} />
+      <h4>{item.title}</h4>
+      <p>{item.short_description}</p>
     </div>
   );
 }
